e882d67650d7873ac9f8463475f17a6e72d513dc
wangmy <1352546265@qq.com>
2024-05-07 15:43:02 +0800

    feature: support sequential matrix multiply

---
+++ src/matrix.rs
@@ -0,0 +1,91 @@
+use anyhow::{anyhow, Result};
+use std::{
+    fmt,
+    ops::{Add, AddAssign, Mul},
+};
+
+pub struct Matrix<T> {
+    data: Vec<T>,
+    row: usize,
+    col: usize,
+}
+
+impl<T> Matrix<T> {
+    pub fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
+        Self {
+            data: data.into(),
+            row,
+            col,
+        }
+    }
+}
+
+impl<T> fmt::Display for Matrix<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{{")?;
+        for i in 0..self.row {
+            for j in 0..self.col {
+                write!(f, "{}", self.data[i * self.col + j])?;
+                if j != self.col - 1 {
+                    write!(f, " ")?;
+                }
+            }
+            if i != self.row - 1 {
+                write!(f, ", ")?
+            }
+        }
+        write!(f, "}}")?;
+
+        Ok(())
+    }
+}
+
+impl<T> fmt::Debug for Matrix<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Matrix(row={}, col={}, {})", self.row, self.col, self)
+    }
+}
+
+pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
+where
+    T: Copy + Default + Add<Output = T> + AddAssign + Mul<Output = T>,
+{
+    if a.col != b.row {
+        return Err(anyhow!("Matrix multiply error: a.col != b.row"));
+    }
+
+    let mut data = vec![T::default(); a.row * b.col];
+    for i in 0..a.row {
+        for j in 0..b.col {
+            for k in 0..a.col {
+                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            }
+        }
+    }
+
+    Ok(Matrix {
+        data,
+        row: a.row,
+        col: b.col,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_matrix_multiply() -> Result<()> {
+        let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
+        let c = multiply(&a, &b)?;
+
+        assert_eq!(c.data, &[22, 28, 49, 64]);
+        Ok(())
+    }
+}
